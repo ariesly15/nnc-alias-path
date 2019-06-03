ac1dcedf206d7ed3c537cda394878a11b79e0715
aweleey <liyidyx@gmail.com>
2019-06-03 13:08:35 +0800

    opti: up

--- extension.js
+++ extension.js
@@ -3,10 +3,7 @@
  */
 function activate(context) {
     console.log("恭喜，您的扩展 “nnc-alias-path” 已被激活！");
-    console.log("恭喜，您的扩展 “nnc-alias-path” 已被激活！");
-    console.log("恭喜，您的扩展 “nnc-alias-path” 已被激活！");
 
-    require("./src/hover")(context); // 悬停提示
     require("./src/alias")(context); // 跳转到定义
 }
 
